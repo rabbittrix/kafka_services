a039a0dd5f6745f419d07d56c35e33bf34aa3212
rabbittrix <rabbittrix@hotmail.com>
2022-06-11 09:51:08 +0100

    Create str-consumer

---
+++ str-consumer/src/main/java/com/jrsf/strconsumer/config/StringConsumerConfig.java
@@ -0,0 +1,28 @@
+package com.jrsf.strconsumer.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+@Configuration
+public class StringConsumerConfig {
+    private final KafkaProperties properties;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        var config = new HashMap<String, Object>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+}
