7b4aa95512e69f1f6c2ca621df375b68134156dd
rabbittrix <rabbittrix@hotmail.com>
2022-06-11 15:21:26 +0100

    Using interceptor in business rule

--- str-consumer/src/main/java/com/jrsf/strconsumer/listeners/StrConsumeListener.java
+++ str-consumer/src/main/java/com/jrsf/strconsumer/listeners/StrConsumeListener.java
@@ -2,6 +2,7 @@ package com.jrsf.strconsumer.listeners;
 
 import com.jrsf.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Log4j2
@@ -17,7 +18,8 @@ public class StrConsumeListener {
         log.info("LOG ::: Received message {} ", message);
     }
 
-    @StrConsumerCustomListener(groupId = "str-group-1")
+    //Using interceptor in business rule
+    @KafkaListener(groupId = "str-group-1", topics = "sysfacturing-topic", containerFactory = "validMessageContainerFactory")
     public void history(String message) {
         log.info("HISTORY ::: Received message {} ", message);
     }
