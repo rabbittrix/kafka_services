073672ea2490cdaad5982a99654c9e47fbf94b65
rabbittrix <rabbittrix@hotmail.com>
2022-06-12 08:19:27 +0100

    create errorHandler

---
+++ str-consumer/src/main/java/com/jrsf/strconsumer/exceptions/ErrorCustomHandler.java
@@ -0,0 +1,23 @@
+package com.jrsf.strconsumer.exceptions;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.listener.ListenerExecutionFailedException;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class ErrorCustomHandler implements KafkaListenerErrorHandler {
+
+    @Override
+    public Object handleError(Message<?> message, ListenerExecutionFailedException e) {
+        log.info("EXCEPTION_HANDLER ::: Received message");
+        return null;
+    }
+
+//    @Override
+//    public Object handleError(Message<?> message, ListenerExecutionFailedException exception, Consumer<?, ?> consumer) {
+//        return KafkaListenerErrorHandler.super.handleError(message, exception, consumer);
+//    }
+}
