ead3a100948c6253f87096360295e1e041c1db70
rabbittrix <rabbittrix@hotmail.com>
2022-06-14 18:46:29 +0100

    create consumer json

---
+++ json-consumer/src/test/java/com/jrsf/jsonconsumer/JsonConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrsf.jsonconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
